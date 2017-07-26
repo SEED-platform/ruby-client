a4bd74dacb3c08d4ee59dc56b74dd45809c8cbd7
Nicholas Long <nicholas.long@nrel.gov>
2017-07-26 12:39:29 -0600

    initial code

---
+++ lib/seed.rb
@@ -0,0 +1,8 @@
+require 'rest-client'
+require 'json'
+require 'base64'
+
+require_relative 'seed/version'
+require_relative 'seed/organization'
+require_relative 'seed/cycle'
+require_relative 'seed/api'
