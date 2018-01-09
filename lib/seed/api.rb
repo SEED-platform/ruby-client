b4e625687e8963d0f51b3a2f64509ce7cdb5020b
Nicholas Long <nicholas.long@nrel.gov>
2018-01-09 14:36:20 -0700

    this is not python

--- lib/seed/api.rb
+++ lib/seed/api.rb
@@ -270,11 +270,9 @@ module Seed
       uri = URI.escape("#{@host}/v2/properties/#{property_id}/?cycle_id=#{@cycle_obj.id}&organization_id=#{@organization.id}")
       RestClient.put(uri, payload.to_json, authorization: @api_header, content_type: :json) do |response, _request, result|
         if result.code.to_i == 200
-          response = JSON.parse(response, symbolize_names: true)
-          return True, Property.from_hash(response[:state])
+          return true, {}
         elsif result.code.to_i == 204
-          response = JSON.parse(response, symbolize_names: true)
-          return True, response
+          return true, response
         elsif result.code.to_i == 422
           return false, response
         else
